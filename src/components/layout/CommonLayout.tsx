5eaeed907bc16dcae14a5bb5fea6d8eb789b0755
Tanvir Ahmed <tanvir03.2001@gmail.com>
2025-08-14 02:12:48 +0600

    Project Setup with shadcn ui

---
+++ src/components/layout/CommonLayout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+interface IProps {
+  children: ReactNode;
+}
+
+export default function CommonLayout({ children }: IProps) {
+  return (
+    <div className=" min-h-screen flex flex-col">
+      <Navbar />
+      <div className="grow-1">{children}</div>
+      <Footer />
+    </div>
+  );
+}
